9d9b0a1fe7118c12271cd78ef6ddbec086879b83
edurin <75543491+edurin@users.noreply.github.com>
2021-01-31 00:08:49 +0700

    Update test_world_download.sh

--- test_world_download.sh
+++ test_world_download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone https://github.com/edurin/educational_worlds.git
 cd educational_worlds/
-cp -rf models/ /home/clover/.gazebo/models/
+cp -rf models/ /home/clover/.gazebo/
 cp -f simulator.launch /home/clover/catkin_ws/src/clover/clover_simulation/launch/
 cp -f parquet.jpg /home/clover/catkin_ws/src/clover/clover_simulation/models/parquet_plane/materials/textures/
 cp -f shirokaya_1.world /home/clover/catkin_ws/src/clover/clover_simulation/resources/worlds/
